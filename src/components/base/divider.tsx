a0793f369abba9f7d811c87aae91cda58879cf25
Andrei U <Upolovnew@gmail.com>
2022-12-15 10:43:42 +0300

    fix: divider element

--- src/components/base/divider.tsx
+++ src/components/base/divider.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import style from './divider.module.scss';
-import { IDivider } from "./divider.props";
-import cn from 'classnames';
+import React, {DetailedHTMLProps, HTMLAttributes} from 'react'
+import style from './divider.module.scss'
+import cn from 'classnames'
 
+export interface DividerProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-const Divider = ({className, ...props}: IDivider) => {
-  return (
-    <div className={cn(style.main, className)}/>
-  )
+const Divider: React.FC<DividerProps> = ({
+  className,
+  ...props
+}): JSX.Element => {
+  return <div className={cn(style.main, className)} />
 }
 
 export default Divider
