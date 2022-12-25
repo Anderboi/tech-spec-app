27876ebd1a49133112d561708cd7ece061f79c58
Andrei U <Upolovnew@gmail.com>
2022-12-25 14:25:41 +0300

    refactor: redirect main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 import {ThemeProvider} from 'next-themes'
 
 export default function App({Component, pageProps}: AppProps) {
+
   return (
     <ThemeProvider
         enableSystem
